08e88a1edf6872130d03fcb6a5cdf5bcb29a3c0d
Mikael-Rt-JS <61776022+Mikael-Rt-JS@users.noreply.github.com>
2024-05-13 02:43:17 +0400

    worker slide
    html css

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+  <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;600;700;900&amp;display=swap" rel="stylesheet">
+
+  <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-brands/css/uicons-brands.css'>
+
+  <title>replit</title>
+  <!-- <link href="./css/root.css" rel="stylesheet" type="text/css" /> -->
+  <link href="./assets/css/global.css" rel="stylesheet" type="text/css" />
+  <link href="./assets/css/slider_block.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+  <div class="flex_slider">
+    <div class="item item1">
+      <span>Amarr</span>
+    </div>
+    <div class="item item2">
+      <span>Caldari</span>
+    </div>
+    <div class="item item3">
+      <span>Galente</span>
+    </div>
+    <div class="item item4">
+      <span>Minmatar</span>
+    </div>
+  </div>
+  <!-- <script src="script.js"></script> -->
+</body>
+
+</html>
